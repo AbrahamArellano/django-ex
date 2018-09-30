4468ee81623cdba4969c6ad223190fac00ecccd3
aarellan <aarellan@localhost.localdomain>
2018-10-01 00:09:51 +0200

    Route request and namespace request

--- openshift_client_no_container/dynamic_openshift_client.py
+++ openshift_client_no_container/dynamic_openshift_client.py
@@ -1,3 +1,4 @@
+import os
 import yaml
 from kubernetes import client, config
 from openshift.dynamic import DynamicClient
@@ -7,20 +8,23 @@ dyn_client = DynamicClient(k8s_client)
 
 v1_routes = dyn_client.resources.get(api_version='route.openshift.io/v1', kind='Route')
 
-route = """
-apiVersion: route.openshift.io/v1
-kind: Route
-metadata:
-  name: redhat-route
-spec:
-  host: basic-rest-client.rest-project.svc
-  to:
-    kind: Service
-    name: basic-rest-client
-"""
+project_namespace = input("Provide Openshift namespace: ")
+route_list = v1_routes.get(namespace=project_namespace)
 
-route_data = yaml.load(route)
-resp = v1_routes.create(body=route_data, namespace='rest-project')
+print("+++++++++++++++")
+print(route_list)
+print("+++++++++++++++")
 
-# resp is a ResourceInstance object
-print(resp.metadata)
+for route in route_list.items:
+	print("Route:")
+	print("Name: " + route.metadata.name)
+	if route.spec.tls is not None:
+		if route.spec.tls.certificate != None:
+			print("Certificate: " + route.spec.tls.certificate)
+		if route.spec.tls.key != None:
+			print("Key: " + route.spec.tls.key)
+		if route.spec.tls.caCertificate != None:
+			print("CA Certificate: " + route.spec.tls.caCertificate)
+		if route.spec.tls.destinationCACert != None:
+			print("Destination CA Certificate: " + route.spec.tls.destinationCACert)
+	print("+++++++++++++++")
